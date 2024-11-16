3f3bab6bf7514875c71cf6033aa1a8f0430eedcc
LimitsAtInfinity <limitsatinfinity@syn-2603-8080-9000-f67a-0000-0000-0000-1f07.res6.spectrum.com>
2024-11-15 22:24:08 -0700

    added winner functionality

--- hangman.py
+++ hangman.py
@@ -1,18 +1,40 @@
 import random
 def main():
-    words = ['papa', 'mama', 'speaker']
+    words = [
+    # Simple words
+    "apple", "banana", "cherry", "grape", "orange",
+    "table", "chair", "window", "pencil", "paper",
+    "school", "garden", "bridge", "mountain", "river",
+    "family", "friend", "summer", "winter", "autumn",
+    "music", "movie", "planet", "ocean", "desert",
+    "flower", "animal", "cloud", "mirror", "travel",
+
+    # More complex words
+    "mystery", "ancient", "whisper", "journey", "invisible",
+    "adventure", "memorable", "crescent", "treasure", "phantom",
+    "benevolent", "solitude", "lighthouse", "astronomy", "wilderness",
+    "paradise", "horizon", "sanctuary", "labyrinth", "serenity",
+    "ephemeral", "kaleidoscope", "perseverance", "phenomenon", "quarantine",
+    "renaissance", "allegiance", "catastrophe", "magnificent", "endeavor",
+    "symmetry", "vocabulary", "universe", "metaphor", "silhouette"
+]
+
     winner = False
     dict_word = {}
     blank_word = []
     random_word = random.choice(words)
+
     for letter in random_word:
         blank_word.append('_')
 
     for letter in blank_word:
         print(letter, end='')
+    print()
+    print(len(blank_word))
     print(hangmanart()[0])
 
     strike = 0
+    points = 0
     while(winner == False):
         guess = input("Enter your guess, one letter at a time: ")
         for i in range(len(random_word)):
@@ -22,17 +44,21 @@ def main():
                 break
             elif guess == random_word[i]:
                 blank_word[i] = guess
+                points += 1
 
         for letter in blank_word:
             print(letter, end='')
         print()  
-        print('Strikes', strike)
+        print('Strikes: ', strike)
+        print('Points: ', points)
 
         if strike == 5:
             print("Strikes", strike)
             print("The Pirate is dead!")
             break
-
+        elif points == len(blank_word):
+            winner = True
+            print("You won")
 
 def hangmanart():
     hangmanart = ['''
